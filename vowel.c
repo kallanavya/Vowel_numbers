0357b0a3bf2704fa960db667b4369c5bbb480c5e
kallanavya <114546855+kallanavya@users.noreply.github.com>
2022-09-28 07:38:16 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    int uppercase_vowel,lowercase_vowel;
+    printf("Enter the alphabet= ");
+    scanf("%c",&c);
+    
+    uppercase_vowel=(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    lowercase_vowel= ( c == 'a' || c== 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    if(uppercase_vowel || lowercase_vowel)
+    {
+        printf(" %c is vowel ",c);
+    }
+    else{
+        printf("%c is consonant ",c);
+    }
+    return 0;
+}
